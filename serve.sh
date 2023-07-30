70d3cc75107d35605851e087c66119fe24169de1
ssatosays <ssatosays@gmail.com>
2023-07-30 19:08:28 +0900

    Create serve.sh

---
+++ serve.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Run the Hugo server on localhost
+hugo serve
+
+# If you want to include drafts on localhost, uncomment the line below
+# Run the Hugo server on localhost including draft pages
+# hugo serve -D
